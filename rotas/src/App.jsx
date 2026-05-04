831d7225d768eb287dcd4956323e69ef3075c61a
jennifer <jennifer.k.silva7@aluno.senai.br>
2026-05-04 11:53:29 -0300

    first commit

---
+++ rotas/src/App.jsx
@@ -0,0 +1,32 @@
+// Editaremos aqui
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Sobre from './pages/Sobre/Sobre';
+import Home from './pages/Home/Home';
+import Servicos from './pages/Servicos/Servicos';
+import Contato from './pages/Contato/Contato';
+import Extra from './pages/Extra/Extra';
+import NotFound from './pages/NotFound/NotFound';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+function App() {
+  return (
+    // Editaremos aqui
+    <BrowserRouter>
+    <Navbar/>
+      <main style={{minHeight: '80vh',padding: '20px'}}>
+    <Routes> 
+      <Route path='/' element={<Home/>} />
+      <Route path='/sobre' element={<Sobre/>} />
+      <Route path='/servicos' element={<Servicos/>} />
+      <Route path='/contato' element={<Contato/>} />
+      <Route path='/extra' element={<Extra/>} />
+      <Route path='*' element={<NotFound/>} />
+    </Routes>
+      </main>
+      <Footer/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
